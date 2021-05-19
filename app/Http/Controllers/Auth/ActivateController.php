1392eee5629c42f7926c595780897bb3dfd87743
greendream21 <uri.dev.0802@gmail.com>
2021-05-19 09:44:53 +0000

    Add send info to MailChimp
    (cherry picked from commit 1ebf1c19ad6cc474ee9a829fc421fdcc6a480c2c)

--- app/Http/Controllers/Auth/ActivateController.php
+++ app/Http/Controllers/Auth/ActivateController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use \DrewM\MailChimp\MailChimp;
 
 class ActivateController extends Controller
 {
@@ -27,7 +28,22 @@ class ActivateController extends Controller
 
         $user->activated_at = now();
         $user->save();
+        
+        $MailChimp = new MailChimp(env('MAILCHIMP_API'));
+        $result = $MailChimp->get('lists');
 
+        $list_id = $result['lists'][0]['id'];
+
+        $result = $MailChimp->post("lists/$list_id/members", [
+            'email_address' => $user->email,
+            'status' => 'subscribed',
+            'tags' => [env('MAILCHIMP_TAG')],
+            'merge_fields' => [
+                'FNAME' =>  $user->name,
+                'LNAME' =>  $user->last_name
+            ]
+        ]);
+        
         return response([
             'success' => true,
             'message' => 'Your account successfully activated! You can login to your account now.'
