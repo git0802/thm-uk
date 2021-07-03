e2a738f9c7ef631f31ae18f233c0c7db573b4180
greendream21 <uri.dev.0802@gmail.com>
2021-07-03 12:17:38 +0530

    Facebook pixel code update

--- config/facebook-pixel.php
+++ config/facebook-pixel.php
@@ -4,8 +4,9 @@ return [
 
     /*
      * The Facebook Pixel id, should be a code that looks something like "XXXXXXXXXXXXXXXX".
+     * For UK Site: 2876202345992402
      */
-    'facebook_pixel_id' => '303641300690369',
+    'facebook_pixel_id' => env('FACEBOOK_PIXEL_ID', '2876202345992402'),
 
     /*
      * Enable or disable script rendering. Useful for local development.
